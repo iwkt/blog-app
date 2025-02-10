86cdaf612a6b92b277582ff7726f9d1614c307c3
miku-nosuke <iwk@mac.com>
2025-02-11 06:25:57 +0900

    seedを使ってダミーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Article.create(title: 'Hello World', content: 'This is my first article')
+Article.create(title: '今日は建国記念の日', content: '初代天皇が即位した日')
