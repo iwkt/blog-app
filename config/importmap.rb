21ea5b54447d72e72376d1259ea30c72c802b9b8
miku-nosuke <iwk@mac.com>
2025-03-09 09:31:45 +0900

    delete が効かない問題を解決

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+# Pin npm packages by running ./bin/importmap
 
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "application"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
